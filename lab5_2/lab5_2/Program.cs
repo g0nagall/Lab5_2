fced9ee4967992245eba427875ef47d3cc458f05
g0nagall <50554188+g0nagall@users.noreply.github.com>
2019-05-20 03:54:32 +0300

    Add files via upload

---
+++ lab5_2/lab5_2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace lab5_1
+{
+	class Program
+	{
+		class A
+		{
+			public int a;
+			public int b;
+			public int C => a > b ? a - b : b - a;
+
+			// Default constructor.
+			public A()
+			{
+				a = 10;
+				b = 15;
+			}
+
+			public A(int a, int b)
+			{
+				this.a = a;
+				this.b = b;
+			}
+		}
+		class B : A
+		{
+			public int d;
+			public int C2 => a + b + d;
+
+			public B() { }
+
+			public B(int d)
+			: this(d: d, a: 0, b: 0) { }
+
+			public B(int a, int b, int d) : base(a, b) => this.d = d;
+
+		}
+		static void Main(string[] args)
+			{
+			A a = new A();
+			Console.WriteLine(a.C);
+			B b = new B(3);
+
+			Console.WriteLine($"a = {b.a}, b = {b.b}, d = {b.d}");
+		}
+	
+}
+}
